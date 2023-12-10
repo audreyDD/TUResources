89476db824cb32dee33328f8a7e485ff4d05c296
Jennalynn Morton <jmorton1@trinity.edu>
2023-12-09 19:34:35 -0600

    validateUser works (make sure to be on pandora06)

--- server/app/models/DataModel.scala
+++ server/app/models/DataModel.scala
@@ -7,6 +7,7 @@ import scala.concurrent.Future
 
 class DataModel(db: Database)(implicit ec: ExecutionContext) {
     def validateUser(username: String, password: String): Future[Option[Int]] = {
+      println("validateUser() in DataModel")
         val matches = db.run(Users.filter(userRow => userRow.username === username && userRow.password === password).result)
         matches.map(userRows => userRows.headOption.flatMap { userRow =>
             Some(userRow.id)
