b4a034237e9fed4ba302fdadab380192059fc609
Jennalynn Morton <jmorton1@trinity.edu>
2023-12-10 19:50:32 -0600

    UI updated and logout functionality

--- server/public/javascript/website.js
+++ server/public/javascript/website.js
@@ -17,24 +17,42 @@ function login() {
         body: JSON.stringify({ username, password })
     }).then(res => res.json()).then(data => {
         if(data) {
+            document.getElementById("message").hidden=true;
             document.getElementById("login-section").hidden = true;
             document.getElementById("create-user-section").hidden = true;
             document.getElementById('home-page').style.display = 'flex';
-            document.getElementById("logout").hidden = false;
             document.getElementsByTagName("TITLE")[0].text="Resources";
         } else {
+            document.getElementById("message").hidden=false;
+            document.getElementById("message").innerHTML="Incorrect Login Information";
             console.log("failed validate");
         }
     })
 }
+function logout(){
+    document.getElementById("message").hidden=true;
+    document.getElementById('home-page').style.display = 'none';
+    goToLogin();
+    //TODO actually logout
+    console.log("logout in js");
+}
 
 function goToCreateUser(){
+    document.getElementById("message").hidden=true;
+    document.getElementsByTagName("TITLE")[0].text="Create User";
     document.getElementById("login-section").hidden = true;
     document.getElementById("create-user-section").hidden = false;
 
 }
+function goToLogin(){
+    document.getElementById("message").hidden=true;
+    document.getElementsByTagName("TITLE")[0].text="Login";
+    document.getElementById("login-section").hidden = false;
+    document.getElementById("create-user-section").hidden = true;
 
+}
 function createUser() {
+    document.getElementById("message").hidden=true;
     const username = document.getElementById("username-create").value;
     const password = document.getElementById("password-create").value;
     console.log("createUser() in js");
@@ -47,15 +65,17 @@ function createUser() {
             // document.getElementById("login-section").hidden = true;
             document.getElementById("create-user-section").hidden = true;
             document.getElementById('home-page').style.display = 'flex';
-            document.getElementById("logout").hidden = false;
             document.getElementsByTagName("TITLE")[0].text="Resources";
         } else {
+            document.getElementById("message").hidden=false;
+            document.getElementById("message").innerHTML="Username Taken";
             console.log("failed to create");
         }
     })
 }
 function showHomePage() {
 
+    document.getElementById("message").hidden=true;
     document.getElementsByTagName("TITLE")[0].text="Resources";
     document.getElementById('login-section').style.display = 'none';
     document.getElementById('create-user-section').style.display = 'none';
@@ -64,6 +84,7 @@ function showHomePage() {
 
 
 function openCategory(category) {
+    document.getElementById("message").hidden=true;
     console.log("openCategory in js");
     hideHomePage()
     const resourceTable = document.getElementById("resource-table")
@@ -79,11 +100,10 @@ function openCategory(category) {
         headers: {'Content-Type': 'application/json', 'Csrf-Token': csrfToken},
         body: JSON.stringify(category)
     }).then(res => res.json()).then(resources => {
-        //TODO: Make button to "schedule" each resource
         if (resources) {
             document.getElementsByTagName("TITLE")[0].text=category;
             
-            console.log(resources);
+            //console.log(resources);
             document.getElementById("home-page").hidden = true
             document.getElementById("resource-page").hidden = false
             
@@ -101,7 +121,7 @@ function openCategory(category) {
 
             for(const resource of resources) {
                 //console.log("here is a resource");
-                console.log(resource);
+                //console.log(resource);
                 let tr = document.createElement("tr")
     
                 let resourceName = document.createTextNode(resource.name)
@@ -137,6 +157,7 @@ function openCategory(category) {
 }
 
 function back() {
+    document.getElementById("message").hidden=true;
     
     document.getElementsByTagName("TITLE")[0].text="Resources";
     document.getElementById("home-page").style.display = 'flex'
@@ -144,15 +165,18 @@ function back() {
 }
 
 function hideHomePage() {
+    document.getElementById("message").hidden=true;
     document.getElementById("home-page").style.display = 'none'
     document.getElementById("resource-page").hidden = false
     
 }
 
 function schedule(category, resourceName) {
+    document.getElementById("message").hidden=true;
 
     document.getElementsByTagName("TITLE")[0].text="Scheduling";
     console.log(resourceName)
+    document.getElementById("logout").hidden = true
     document.getElementById("home-page").hidden = true
     document.getElementById("resource-page").hidden = true
     document.getElementById("schedule-page").hidden = false
@@ -163,8 +187,10 @@ function schedule(category, resourceName) {
         body: JSON.stringify({category, resourceName})
     }).then(res => res.json()).then(resource => {
         if(resource) {
-            console.log("fetching" + resource)
+            console.log("fetching" + resource);
         } else {
+            document.getElementById("message").hidden=false;
+            document.getElementById("message").innerHTML="Invalid Time Slot for Chosen Resource";
             console.log("failed schedule fetch")
         }
     })
